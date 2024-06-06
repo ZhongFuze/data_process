2fbe282ae92f75945e8afea7ed3830cdcd587be2
ZhongFuze <zella@mask.io>
2024-06-06 19:39:04 +0800

    [#] add `Clusters` get_name & get_address controller

---
+++ src/controller/clusters_controller.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author: Zella Zhong
+Date: 2024-06-06 15:17:04
+LastEditors: Zella Zhong
+LastEditTime: 2024-06-06 19:38:37
+FilePath: /data_process/src/controller/clusters_controller.py
+Description: 
+'''
+import logging
+from datetime import datetime
+
+from httpsvr import httpsvr
+from setting import get_conn
+
+
+def dict_factory(cursor, row):
+    """
+    Convert query result to a dictionary.
+    """
+    col_names = [col_desc[0] for col_desc in cursor.description]
+    row_dict = dict(zip(col_names, row))
+    for key, value in row_dict.items():
+        if isinstance(value, datetime):
+            row_dict[key] = int(value.timestamp())
+    return row_dict
+
+
+class ClustersController(httpsvr.BaseController):
+    '''ClustersController'''
+    def __init__(self, obj, param=None):
+        super(ClustersController, self).__init__(obj)
+
+    def get_name(self):
+        '''Get the name of the cluster by address'''
+        address = self.inout.get_argument("address", "")
+        logging.debug("Clusters get_name?address={}".format(address))
+        if address == "":
+            return httpsvr.Resp(msg="Invalid address", data=[], code=-1)
+        rows = []
+        code = 0
+        msg = ""
+        try:
+            pg_conn = get_conn()
+            cursor = pg_conn.cursor()
+            ssql = """
+                SELECT address, type, clustername, name, isverified, updatedat
+                FROM public.clusters_name WHERE address='{}' clusterName is not null AND isverified=true
+            """
+            cursor.execute(ssql.format(address))
+            # logging.debug(ssql.format(address))
+            rows = [dict_factory(cursor, row) for row in cursor.fetchall()]
+            cursor.close()
+            pg_conn.close()
+        except Exception as e:
+            code = -1
+            msg = repr(e)
+            logging.exception(e)
+
+        return httpsvr.Resp(msg=msg, data=rows, code=code)
+
+
+    def get_address(self):
+        '''Get the address by name=clusters/main'''
+        name = self.inout.get_argument("name", "")
+        logging.debug("Clusters get_address?name={}".format(name))
+        if name == "":
+            return httpsvr.Resp(msg="Invalid name", data=[], code=-1)
+
+        rows = []
+        code = 0
+        msg = ""
+
+        ssql = ""
+        if name.find("/") != -1:
+            cluster_name = name.split("/")[0]
+            ssql += """SELECT address, type, clustername, name, isverified, updatedat FROM public.clusters_name
+                    WHERE clustername='{}' AND isverified=true
+            """
+            ssql = ssql.format(cluster_name)
+        else:
+            ssql += """SELECT address, type, clustername, name, isverified, updatedat FROM public.clusters_name
+                    WHERE clustername='{}' AND isverified=true
+            """
+            ssql = ssql.format(name)
+
+        if ssql == "":
+            return httpsvr.Resp(msg="Invalid name format", data=[], code=-1)
+
+        try:
+            pg_conn = get_conn()
+            cursor = pg_conn.cursor()
+            cursor.execute(ssql)
+            # logging.debug(ssql)
+            rows = [dict_factory(cursor, row) for row in cursor.fetchall()]
+            cursor.close()
+            pg_conn.close()
+        except Exception as e:
+            code = -1
+            msg = repr(e)
+            logging.exception(e)
+
+        return httpsvr.Resp(msg=msg, data=rows, code=code)
--- src/data_server.py
+++ src/data_server.py
@@ -4,7 +4,7 @@
 Author: Zella Zhong
 Date: 2023-11-22 19:59:14
 LastEditors: Zella Zhong
-LastEditTime: 2024-05-21 18:10:08
+LastEditTime: 2024-06-06 19:19:37
 FilePath: /data_process/src/data_server.py
 Description: 
 '''
@@ -18,6 +18,7 @@ from controller.mydata_controller import MyDataController
 from controller.keybase_controller import KeybaseController
 from controller.genome_controller import GenomeController
 from controller.aggregation_controller import AggregationController
+from controller.clusters_controller import ClustersController
 
 
 if __name__ == "__main__":
@@ -38,6 +39,8 @@ if __name__ == "__main__":
             ["/data_server/genome/get_address", GenomeController, "get_address"],
             ["/data_server/aggregation/search", AggregationController, "search"],
             ["/data_server/aggregation/opensea_account", AggregationController, "opensea_account"],
+            ["/data_server/clusters/get_name", ClustersController, "get_name"],
+            ["/data_server/clusters/get_address", ClustersController, "get_address"],
         ]
         svr = httpsvr.HttpSvr(config, ctrl_info)
         svr.Start()
